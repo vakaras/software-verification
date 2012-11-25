fb8b29a2b9c0b72d83ffa626612b241141796fb2
Vytautas Astrauskas <vastrauskas@gmail.com>
2012-11-25 23:02:51 +0100

    Continuing…

--- code/experiments/dfs3.c
+++ code/experiments/dfs3.c
@@ -50,16 +50,16 @@ inductive parents_i = parents_nil |
 predicate parents_p(
     struct node *node,
     int distance,
-    parents_i parents) =
+    parents_i parents,
+    struct node *parent) =
   node == 0 ?
   parents == parents_nil &*&
   distance == -1 :
-  node->parent |-> ?parent &*&
+  node->parent |-> parent &*&
   node->distance |-> distance &*&
-  parents_p(parent, ?parent_distance, ?parent_parents) &*&
+  parents_p(parent, ?parent_distance, ?parent_parents, ?parent_parent) &*&
   parents == parents_cons(node, distance, parent_parents) &*&
   parent_distance + 1 == distance;
-
 
 @*/
 
@@ -74,29 +74,29 @@ predicate visited_child_p(
   container != 0;
 
 predicate visited_children_p(struct list_node *container, children_i children) =
-  children == children_nil ?
-  emp :
+  container == 0 ?
+  children == children_nil :
   visited_child_p(container, ?next, ?node) &*&
   visited_node_p(node, _, _, _) &*&
   visited_children_p(next, ?tail) &*&
   children == children_cons(node, tail);
 
-lemma void visited_children_append(
-    struct list_node *start,
-    struct list_node *new)
-  requires  visited_children_p(start, ?children) &*&
-            visited_child_p(new, ?next, ?node) &*&
-            visited_node_p(node, _, _, _) &*&
-            start != 0;
-  ensures   visited_children_p(start, ?visited_children);
-{
-  // TODO: Ensure that “new” was actually appended.
-  open visited_children_p(start, children);
-  switch (children) {
-    case children_nil:
-      open visited_child_p(new, next, node);
-      close visited_children_p(new->next, children_nil);
-  }
+//lemma void visited_children_append(
+//    struct list_node *start,
+//    struct list_node *new)
+//  requires  visited_children_p(start, ?children) &*&
+//            visited_child_p(new, ?next, ?node) &*&
+//            visited_node_p(node, _, _, _) &*&
+//            start != 0;
+//  ensures   visited_children_p(start, ?visited_children);
+//{
+//  // TODO: Ensure that “new” was actually appended.
+//  open visited_children_p(start, children);
+//  switch (children) {
+//    case children_nil:
+//      open visited_child_p(new, next, node);
+//      close visited_children_p(new->next, children_nil);
+//  }
 //open visited_child_p(start, ?start_next, ?start_node);
 //if (start_next == new) {
 //  open visited_child_p(new, next, node);
@@ -107,7 +107,7 @@ lemma void visited_children_append(
 //  visited_children_append(start->next, new);
 //  close visited_children_p(start, _);
 //}
-}
+//}
   
 predicate visited_node_p(
     struct node *node,
@@ -116,10 +116,10 @@ predicate visited_node_p(
     int distance) =
   node != 0 &*&
   node->children |-> ?container &*&
-  children_p(container, children) &*&
+  visited_children_p(container, children) &*&
   node->distance |-> distance &*&
   node->parent |-> ?parent &*&
-  parents_p(parent, distance - 1, parents);
+  parents_p(parent, distance - 1, parents, _);
 
 @*/
 
@@ -127,42 +127,49 @@ predicate visited_node_p(
 void dfs_worker(struct node *root, int depth, struct node *parent)
   /*@ requires  depth >= 0 &*&
                 node_p(root, ?children) &*&
-                parents_p(parent, depth - 1, ?parents);
+                parents_p(parent, depth - 1, ?parents, ?parent_parent);
   @*/
   /*@ ensures   visited_node_p(root, ?visited_children,
                                parents_cons(root, depth, parents), depth) &*&
                 children == visited_children &*&
-                parents_p(parent, depth - 1, parents);
+                parents_p(parent, depth - 1, parents, parent_parent);
   @*/
 {
   //@ open node_p(root, children);
   root->distance = depth;
   root->parent = parent;
   struct list_node *container = root->children;
-  //@ close visited_children_p(root->children, children_nil);
+  struct list_node *new_list = 0;
+  //@ close visited_children_p(new_list, children_nil);
   while (container != 0)
     /*@ invariant 
                   children_p(container, ?children_loop) &*&
                   root->distance |-> depth &*&
                   root->parent |-> parent &*&
-                  parents_p(parent, depth - 1, parents) &*&
-                  root->children |-> ?children_list &*&
-                  visited_children_p(children_list, ?visited_children_loop);
+                  parents_p(parent, depth - 1, parents, _) &*&
+                  visited_children_p(new_list, ?visited_children_loop);
     @*/
   {
-    //@ close parents_p(root, depth, parents_cons(root, depth, parents));
+    //@ close parents_p(root, depth, parents_cons(root, depth, parents), parent);
     //@ open children_p(container, children_loop);
     //@ open child_p(container, ?next, ?node);
     struct node *child = container->node;
     //@ assert(child == node);
     dfs_worker(child, depth + 1, root);
     struct list_node *tmp = container->next;
-    //@ close visited_child_p(container, next, node);
+    container->next = new_list;
+    //@ close visited_child_p(container, new_list, node);
+    new_list = container;
+    //@ close visited_children_p(new_list, _);
 
     /// close children_p(container, children_loop);
     container = tmp;
-    //@ leak node_p(node, _) &*& visited_child_p(_, next, node);
+    //@ open parents_p(root, depth, parents_cons(root, depth, parents), parent);
   }
+  //@ close parents_p(root, depth, parents_cons(root, depth, parents), parent);
+  //@ open children_p(container, _);
+  root->children = new_list;
+  //@ close visited_node_p(root, _, parents_cons(root, depth, parents), depth);
 }
 
 void dfs(struct node *root)
