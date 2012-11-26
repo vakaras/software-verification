98744cf5e53c8861d10c964030f7bc587675d311
Vytautas Astrauskas <vastrauskas@gmail.com>
2012-11-26 18:31:16 +0100

    Updated to arbitrary amount of children.

--- code/experiments/dfs5.c
+++ code/experiments/dfs5.c
@@ -3,8 +3,8 @@
  * is bigger by one than the distance of the parent.
  */
 struct node {
-  struct node *child_left;
-  struct node *child_right;
+  int children_count;
+  struct node *children;
   struct node *parent;
   int distance;
 };
@@ -13,50 +13,55 @@ struct node {
 
 predicate unvisited_node_p(
     struct node *node,
-    struct node *child_left,
-    struct node *child_right,
+    int children_count,
+    struct node *children,
     struct node *parent,
     int distance) =
   node != 0 &*&
-  node->child_left |-> child_left &*&
-  unvisited_child_p(child_left) &*&
-  node->child_right |-> child_right &*&
-  unvisited_child_p(child_right) &*&
+  node->children_count |-> children_count &*&
+  children_count >= 0 &*&
+  node->children |-> children &*&
+  unvisited_children_p(children, children_count) &*&
   node->parent |-> parent &*&
   parent == 0 &*&
   node->distance |-> distance &*&
   distance == -1;
 
 predicate unvisited_child_p(struct node *child) =
-  child == 0 ?
-  emp :
+  child != 0 &*&
   unvisited_node_p(child, _, _, _, _);
+
+predicate unvisited_children_p(struct node *start, int count) =
+  count == 0 ?
+  emp :
+  unvisited_child_p(start) &*&
+  unvisited_children_p(start + 1, count - 1);
 
 predicate visiting_node_p(
     struct node *node,
-    struct node *child_left,
-    struct node *child_right,
+    int children_count,
+    struct node *children,
     struct node *parent,
     int distance) =
   node != 0 &*&
-  node->child_left |-> child_left &*&
-  unvisited_child_p(child_left) &*&
-  node->child_right |-> child_right &*&
-  unvisited_child_p(child_right) &*&
+  node->children_count |-> children_count &*&
+  children_count >= 0 &*&
+  node->children |->children &*&
+  unvisited_children_p(children, children_count) &*&
   node->parent |-> parent &*&
   node->distance |-> distance;
 
 predicate visited_node_p(
     struct node *node,
-    struct node *child_left,
-    struct node *child_right,
+    int children_count,
+    struct node *children,
     struct node *parent,
     int distance) =
   node != 0 &*&
-  node->child_left |-> child_left &*&
-  visited_child_p(child_left, node, distance + 1) &*&
-  node->child_right |-> child_right &*&
-  visited_child_p(child_right, node, distance + 1) &*&
+  node->children_count |-> children_count &*&
+  children_count >= 0 &*&
+  node->children |->children &*&
+  visited_children_p(children, children_count, node, distance + 1) &*&
   node->parent |-> parent &*&
   node->distance |-> distance;
 
@@ -68,37 +73,84 @@ predicate visited_child_p(
   emp :
   visited_node_p(child, _, _, parent, distance);
 
+predicate visited_children_p(
+    struct node *start,
+    int count,
+    struct node *parent,
+    int distance) =
+  count == 0 ?
+  emp :
+  visited_child_p(start, parent, distance) &*&
+  visited_children_p(start + 1, count - 1, parent, distance);
+
+lemma void visited_children_append(
+    struct node *children,
+    int count)
+  requires  visited_children_p(children, count, ?parent, ?depth) &*&
+            visited_child_p(children + count, parent, depth) &*&
+            count >= 0;
+  ensures   visited_children_p(children, count + 1, parent, depth);
+{
+  open visited_children_p(children, count, parent, depth);
+  if (count == 0) {
+    close visited_children_p(children + 1, 0, parent, depth);
+  } else {
+    visited_children_append(children + 1, count - 1);
+  }
+  close visited_children_p(children, count + 1, parent, depth);
+}
+
 @*/
 
-void dfs(struct node *node, int depth)
+void dfs_worker(struct node *node, int depth)
   /*@ requires  depth >= 0 &*&
-                visiting_node_p(node, ?node_child_left, ?node_child_right,
-                                ?parent, depth);
+                visiting_node_p(node, ?node_children_count,
+                                ?node_children, ?parent, depth);
   @*/
-  /*@ ensures   visited_node_p(node, node_child_left, node_child_right,
-                               parent, depth);
+  /*@ ensures   visited_node_p(node, node_children_count,
+                               node_children, parent, depth);
   @*/
 {
-  //@ open visiting_node_p(node, node_child_left, node_child_right, parent, depth);
-  struct node *child_left = node->child_left;
-  //@ open unvisited_child_p(child_left);
-  if (child_left != 0) {
-    //@ open unvisited_node_p(child_left, _, _, _, _);
-    child_left->parent = node;
-    child_left->distance = depth + 1;
-    //@ close visiting_node_p(child_left, _, _, _, _);
-    dfs(child_left, depth + 1);
+  /*@ open visiting_node_p(node, node_children_count,
+                           node_children, parent, depth);
+  @*/
+  int children_count = node->children_count;
+  struct node *children = node->children;
+  int i = 0;
+  //@ close visited_children_p(children, 0, node, depth + 1);
+  for (; i != children_count; i++)
+    /*@ invariant unvisited_children_p(children + i, children_count - i) &*&
+                  visited_children_p(children, i, node, depth + 1) &*&
+                  i >= 0;
+    @*/
+  {
+    //@ open unvisited_children_p(children + i, children_count - i);
+    struct node *child = children + i;
+    //@ open unvisited_child_p(child);
+    //@ open unvisited_node_p(child, _, _, _, _);
+    child->parent = node;
+    child->distance = depth + 1;
+    //@ close visiting_node_p(child, _, _, node, depth + 1);
+    dfs_worker(child, depth + 1);
+    //@ close visited_child_p(child, node, depth + 1);
+    //@ visited_children_append(children, i);
   }
-  //@ close visited_child_p(child_left, node, depth + 1);
-  struct node *child_right = node->child_right;
-  //@ open unvisited_child_p(child_right);
-  if (child_right != 0) {
-    //@ open unvisited_node_p(child_right, _, _, _, _);
-    child_right->parent = node;
-    child_right->distance = depth + 1;
-    //@ close visiting_node_p(child_right, _, _, _, _);
-    dfs(child_right, depth + 1);
-  }
-  //@ close visited_child_p(child_right, node, depth + 1);
+  assert(children_count - i == 0);
+  //@ open unvisited_children_p(children + i, children_count - i);
   //@ close visited_node_p(node, _, _, _, depth);
+}
+
+void dfs(struct node *node)
+  /*@ requires  unvisited_node_p(node, ?node_children_count,
+                                ?node_children, _, _);
+  @*/
+  /*@ ensures   visited_node_p(node, node_children_count,
+                               node_children, 0, 0);
+  @*/
+{
+  //@ open unvisited_node_p(node, node_children_count, node_children, _, _);
+  node->parent = 0;
+  node->distance = 0;
+  //@ close visiting_node_p(node, node_children_count, node_children, 0, 0);
+  dfs_worker(node, 0);
 }
